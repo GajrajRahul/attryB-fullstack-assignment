ce5eaab7732d9499cfedb322373b351d704b817f
Rahul <rahulgajraj3497@gmail.com>
2023-06-13 09:53:42 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const connectDb = require("./config/connectDB");
+const dotenv = require('dotenv');
 const app= express();
 
-connectDb()
+dotenv.config();
 
-app.use(cors())
-app.use(express.json())
+connectDb();
+
+app.use(cors());
+app.use(express.json());
 app.use('/api/user', require('./routes/userRoutes'));
-app.use("/api/car", require("./routes/carRoutes") )
+app.use("/api/car", require("./routes/carRoutes"));
 
-app.listen('5050',()=>{
-    console.log("Server is listening on Port 5050")
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on Port ${PORT}`);
 })
